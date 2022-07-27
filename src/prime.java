e4ce724183f306b26ffa9d10ccffed999feea2e9
muneesMohammed <marhaba.munees@gmail.com>
2022-07-27 09:50:31 +0530

    prime number code added

--- src/prime.java
+++ src/prime.java
@@ -1,5 +1,22 @@
+import java.util.Scanner;
+
 public class prime {
     public static void main(String[] args) {
-        System.out.println("enter a number");
+        while (true) {
+            System.out.println("enter a number");
+            Scanner in = new Scanner(System.in);
+            int num = in.nextInt();
+            boolean flag = true;
+            for (int i = 2; i <= num-1; i++) {
+                if (num % i == 0) {
+                    System.out.println(num+" is not prime");
+                    flag = false;
+                   break;
+                }
+                else {
+                    System.out.println(num+" is prime");
+                }
+            }
+        }
     }
 }
