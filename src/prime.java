dcec18b51026a1ca5e951a34b1be710746f063e2
muneesMohammed <marhaba.munees@gmail.com>
2022-07-27 12:07:02 +0530

    rangeprime code added

--- src/prime.java
+++ src/prime.java
@@ -2,27 +2,24 @@ import java.util.Scanner;
 
 public class prime {
     public static void main(String[] args) {
-        while (true) {
+
             System.out.println("enter a number");
             Scanner in = new Scanner(System.in);
             int num = in.nextInt();
+
+        for (int i = 2; i <= num; i++) {
             boolean flag = true;
-            if (num==0||num==1){
-                System.out.println(num+" is not prime");
-            }
-            else {
-            for (int i = 2; i <= num/2; i++) {
-                if (num % i == 0) {
-                    flag = false;
-                break;
+            for (int j = 2; j <= i/2; j++) {
+                if (i % j == 0) {
+                    flag=false;
+                   break;
                 }
+
+                }
+            if (flag==true){
+                System.out.println(i);
             }
-            if (flag==false){
-                System.out.println(num+"is not prime");
+
             }
-            else {
-                System.out.println(num+" is prime");
-            }
-        }
     }
-}}
+}
